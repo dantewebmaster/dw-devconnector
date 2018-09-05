27d71ce53fd4e0df7a0c5df8a6bc032d48ab20e4
Dante Webmaster <dantewebmaster@outlook.com>
2018-09-05 02:05:15 -0300

    FRONT: Initial layout components and routes created

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+	return (
+		<footer className="bg-dark text-white mt-5 p-4 text-center">
+			<p>Copyright &copy; {new Date().getFullYear()} DevConnector</p>
+		</footer>
+	)
+}
