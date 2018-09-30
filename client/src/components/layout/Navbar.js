e67f4492ff4a1699b66829d54a685c47b328594b
Dante Webmaster <dantewebmaster@outlook.com>
2018-09-30 01:29:30 -0300

    FRONT: App bar using Material Ui

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,70 +5,118 @@ import { Link } from 'react-router-dom';
 import { logoutUser } from '../../actions/authActions';
 import { clearCurrentProfile } from '../../actions/profileActions';
 
+// MU Components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+// Icons
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textDecoration: 'none',
+  },
+  avatar: {
+    marginRight: '8px',
+    marginLeft: '8px',
+  },
+  leftIcon: {
+    marginRight: '16px',
+  },
+};
+
 class Navbar extends Component {
+  state = {
+    anchorEl: null,
+  }
 
   onLogoutClick = e => {
     e.preventDefault();
     const { logoutUser, clearCurrentProfile } = this.props;
     clearCurrentProfile();
     logoutUser();
+    this.handleClose();
   };
 
+  onOpenUserMenu = e => this.setState({ anchorEl: e.currentTarget })
+
+  handleClose = () => this.setState({ anchorEl: null });
+
   render() {
-    const { auth: { isAuthenticated, user } } = this.props;
+    const { classes, auth: { isAuthenticated, user } } = this.props;
+
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
 
     const authLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/feed">Feed</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/dashboard">Dashboard</Link>
-        </li>
-        <li className="nav-item">
-          <a href="#!" onClick={this.onLogoutClick} className="nav-link">
-            <img
-              className="rounded-circle mr-2"
-              src={user.avatar}
-              alt={user.name}
-              title="Your image on Gravatar"
-              style={{ width: '30px' }}
-            />
-            Logout
-					</a>
-        </li>
-      </ul>
+      <React.Fragment>
+        <Button component={Link} to="/feed" color="inherit">Feed</Button>
+        <Avatar alt={user.name} src={user.avatar} className={classes.avatar} />
+        <div>
+          <IconButton
+            color="inherit"
+            aria-label="User menu"
+            aria-owns={open ? 'user-menu' : null}
+            aria-haspopup="true"
+            onClick={this.onOpenUserMenu}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="user-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.onLogoutClick}>
+              <ExitToAppIcon className={classes.leftIcon} /> Sair
+            </MenuItem>
+            <MenuItem component={Link} to="/dashboard" onClick={this.handleClose}>
+              <SettingsIcon className={classes.leftIcon} /> Dashboard
+            </MenuItem>
+          </Menu>
+        </div>
+      </React.Fragment>
     );
 
     const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">Sign Up</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-      </ul>
+      <React.Fragment>
+        <Button component={Link} to="/register" color="inherit">Register</Button>
+        <Button component={Link} to="/login" color="inherit">Login</Button>
+      </React.Fragment>
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">DevConnector</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/profiles">Developers</Link>
-              </li>
-            </ul>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.title}
+              component={Link}
+              to="/"
+            >
+              DevConnector
+            </Typography>
+            <Button component={Link} to="/profiles" color="inherit">Developpers</Button>
             {isAuthenticated ? authLinks : guestLinks}
-          </div>
-        </div>
-      </nav>
+          </Toolbar>
+        </AppBar>
+      </div>
     )
   }
 }
@@ -76,10 +124,13 @@ class Navbar extends Component {
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
+export default connect(
+  mapStateToProps, { logoutUser, clearCurrentProfile }
+)(withStyles(styles)(Navbar));
